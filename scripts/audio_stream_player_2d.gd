d35df0646f2f7ea7bc084ab721e767dba6ee9531
Connor <connoreaton1o9@gmail.com>
2026-05-04 18:11:13 -0500

    added music

---
+++ scripts/audio_stream_player_2d.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer2D
+
+func _ready() -> void:
+	#This is how you would chnage the trcak
+	'''
+	$AudioStreamPlayer.stream = load("INSERT SONG HERE")
+	$AudioStreamPlayer.play()
+	'''
+	
